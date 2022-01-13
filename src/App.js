759da75a857942fcf2af5ff84ea9ac274b38b52d
ravi <ravichodvadiya69@gmail.com>
2022-01-13 16:14:27 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import io from "socket.io-client"
 import Chat from "./Chat";
 
-// const socket = io.connect("https://node-socket-io.vercel.app")
-const socket = io.connect("http://localhost:5000/")
+const socket = io.connect("https://node-socket-io.vercel.app")
+// const socket = io.connect("http://localhost:5000/")
 
 function App() {
   const [userName, setUserName] = useState("")
